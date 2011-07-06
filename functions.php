ff3e37fdb576f29083997a1bf52ec1fc28c01a52
Bob Fanger <bfanger@gmail.com>
2011-07-06 12:11:18 +0200

    Split functions.php into namespaces

--- functions.php
+++ functions.php
@@ -5,277 +5,270 @@
  * @package MVC
  */
 
-/**
- * render($component) is hetzelde als een $component->render(), 
- * Maar render($component) geeft *geen* fatal error als er geen render() methode in het object zit.
- */
-function render($component) {
-	if(is_component($component, true)) {
-		$component->render();
-	} 
-}
+namespace {
+	// global namespace (zodat deze functies direct in een template gebruikt kunnen worden)
+	
+	/**
+	 * render($component) is hetzelde als een $component->render(), 
+	 * Maar render($component) geeft *geen* fatal error als er geen render() methode in het object zit.
+	 */
+	function render($component) {
+		if(SledgeHammer\is_valid_component($component)) {
+			$component->render();
+		}
+	}
 
-/**
- * Geeft de uitvoer van een component als string.
- * (Uitvoer zoals emails en header() worden niet afgevangen)
- *
- * @return string
- */
-function component_to_string($component) {
-	if (is_component($component, true)) {
-		ob_start();
-		$component->render();
-		return ob_get_clean();
+	/**
+	 * Geeft de uitvoer van een component als string.
+	 * (Uitvoer zoals emails en header() worden niet afgevangen)
+	 *
+	 * @return string
+	 */
+	function component_to_string($component) {
+		if (SledgeHammer\is_valid_component($component)) {
+			ob_start();
+			$component->render();
+			return ob_get_clean();
+		}
+	}
+
+	/**
+	 * Controleer of de $variable een component compatible object is. (ducktyping)
+	 *
+	 * @param mixed $variable
+	 * @param bool $showNotice   Show errors
+	 * @return bool 
+	 */
+	function is_component(&$component = '__UNDEFINED__') {
+		return (is_object($component) && method_exists($component, 'render'));
+	}
+
+	/**
+	 * Genereer een <script src=""> tag, mits deze al een keer gegenereerd is.
+	 * @param string $src
+	 * @param string $identifier
+	 * @return void
+	 */
+	function javascript_once($src, $identifier = null) {
+		if ($identifier === null) {
+			$identifier = $src;
+		}
+		if (isset($GLOBALS['included_javascript'][$identifier])) {
+			return;
+		}
+		$GLOBALS['included_javascript'][$identifier] = true;
+		echo '<script type="text/javascript" src="'.$src.'"></script>'."\n";
 	}
 }
 
-/**
- * Controleer of de $variable een component compatible object is.
- *
- */
-function is_component(&$variable = '__UNDEFINED__', $show_notice = false) {
-	if (is_object($variable) && method_exists($variable, 'render')) {
-		return true;
-	} elseif ($show_notice) {
-		if (is_object($variable)) {
-			notice('Invalid $component, class "'.get_class($variable).'" must implement a render() method');
+namespace SledgeHammer {
+	
+	/**
+	 * Check if $component is a Component-compatible and otherwise report notices
+	 * 
+	 * @param mixed $variable
+	 * @return $bool
+	 */
+	function is_valid_component(&$component = '__UNDEFINED__') {
+		if (is_component($component)) {
+			return true;
+		} 
+		if (is_object($component)) {
+			notice ('Invalid $component, class "'.get_class($component).'" must implement a render() method');
 		} elseif ($variable == '__UNDEFINED__') {
 			notice('Variable is undefined');
 		} else {
-			notice('Invalid datatype: "'.gettype($variable).'", expecting a Component object');
+			notice('Invalid datatype: "'.gettype($component).'", expecting a Component object');
 		}
+		return false;
 	}
-	return false;
-}
+	
+	/**
+	 * Zet een array om naar xml/html parameters; array('x' => 'y') wordt ' x="y"'
+	 * 
+	 * @param array $parameters
+	 * @param string $charset  De charset van de parameters (voor htmlentities). Standaard wordt de charset van het actieve document gebruikt.
+	 * @return string
+	 */
 
-/**
- * Functie waarmee je vanuit smarty kunt opvragen of de viewport is ingesteld.
- */
-function viewport_exists($viewport) { // [bool]
-	$viewports = explode(' ', $viewport);
-	$pointer = &$GLOBALS['Viewports'];
-	foreach($viewports as $viewport) {
-		if (!isset($pointer[$viewport])) {
-			return false;
+	function implode_xml_parameters($parameterArray, $charset = null) {
+		$xml = '';
+		if ($charset === null) {
+			$charset = $GLOBALS['charset'];
 		}
-		$pointer = &$pointer[$viewport];
+		foreach($parameterArray as $key => $value) {
+			$xml .= ' '.$key.'="'.htmlentities($value, ENT_COMPAT, $charset).'"';
+		}
+		return $xml;
 	}
-	return true;
-}
 
-/**
- * Zet een array om naar xml/html parameters; array('x' => 'y') wordt ' x="y"'
- * 
- * @param array $parameters
- * @param string $charset  De charset van de parameters (voor htmlentities). Standaard wordt de charset van het actieve document gebruikt.
- * @return string
- */
+	/**
+	 * Zet een string met parameters om naar een array.
+	 *' x="y"' wordt  array('x' => 'y') 
+	 * 
+	 * @param string $tag
+	 * @return array
+	 */
+	function explode_xml_parameters($parameterString) {
+		/* De reguliere expressies manier kan niet omgaan met values die geen quotes hebben e.d..
+		if (preg_match_all('/(?P<attr>[a-z]*)=[\"\'](?P<value>[a-zA-Z0-9\/._-]*)[\"\']/', $parameterString, $match)) {
+			foreach ($match['attr'] as $index => $key) {
+				$parameters[$key] = $match['value'][$index];		
+			} 
+		}
+		//*/
+		$parameters = array();
+		$state = 'NAME';
+		// Parse de string via een state-machine
+		while ($parameterString) {
+			switch ($state) {
 
-function implode_xml_parameters($parameterArray, $charset = null) {
-	$xml = '';
-	if ($charset === null) {
-		$charset = $GLOBALS['charset'];
-	}
-	foreach($parameterArray as $key => $value) {
-		$xml .= ' '.$key.'="'.htmlentities($value, ENT_COMPAT, $charset).'"';
-	}
-	return $xml;
-}
+				case 'NAME': // Zoek de attribuut naam.(de tekst voor de '=') 
+					$equalsPos = strpos($parameterString, '=');
+					if (!$equalsPos) { // er zijn geen attributen meer.
+						break 2; // stop met tokenizing
+					}
+					$value = trim(substr($parameterString, 0, $equalsPos));
+					$value = preg_replace('/.*[ \t]/','', $value); // als er een spatie of tab in de naam staat, haal deze (en alles ervoor) weg
+					$attributeName = $value; // attribuutnaam is bekend.
 
-/**
- * Zet een string met parameters om naar een array.
- *' x="y"' wordt  array('x' => 'y') 
- * 
- * @param string $tag
- * @return array
- */
-function explode_xml_parameters($parameterString) {
-	/* De reguliere expressies manier kan niet omgaan met values die geen quotes hebben e.d..
-	if (preg_match_all('/(?P<attr>[a-z]*)=[\"\'](?P<value>[a-zA-Z0-9\/._-]*)[\"\']/', $parameterString, $match)) {
-		foreach ($match['attr'] as $index => $key) {
-			$parameters[$key] = $match['value'][$index];		
-		} 
-	}
-	//*/
-	$parameters = array();
-	$state = 'NAME';
-	// Parse de string via een state-machine
-	while ($parameterString) {
-		switch ($state) {
-
-			case 'NAME': // Zoek de attribuut naam.(de tekst voor de '=') 
-				$equalsPos = strpos($parameterString, '=');
-				if (!$equalsPos) { // er zijn geen attributen meer.
-					break 2; // stop met tokenizing
-				}
-				$value = trim(substr($parameterString, 0, $equalsPos));
-				$value = preg_replace('/.*[ \t]/','', $value); // als er een spatie of tab in de naam staat, haal deze (en alles ervoor) weg
-				$attributeName = $value; // attribuutnaam is bekend.
-
-				$parameterString = ltrim(substr($parameterString, $equalsPos + 1)); // De parameterstring inkorten.
-				$delimiter = substr($parameterString, 0, 1);
-				if ($delimiter != '"' && $delimiter != "'") { // Staan er geen quotes om de value?
-					$delimiter = ' \t>';
-					$escape = '';
-				} else {
-					$parameterString = substr($parameterString, 1); // de quote erafhalen.
-					$escape = '\\'.$delimiter;
-				}
-				$state = 'VALUE';
-				break;
-
-			case 'VALUE':			
-				if (preg_match('/^([^'.$delimiter.']*)['.$delimiter.']/', $parameterString, $match)) {
-					$parameters[$attributeName] = $match[1]; // De waarde is bekend.
-					$parameterString = substr($parameterString, strlen($match[0]));
-					$state = 'NAME';
+					$parameterString = ltrim(substr($parameterString, $equalsPos + 1)); // De parameterstring inkorten.
+					$delimiter = substr($parameterString, 0, 1);
+					if ($delimiter != '"' && $delimiter != "'") { // Staan er geen quotes om de value?
+						$delimiter = ' \t>';
+						$escape = '';
+					} else {
+						$parameterString = substr($parameterString, 1); // de quote erafhalen.
+						$escape = '\\'.$delimiter;
+					}
+					$state = 'VALUE';
 					break;
-				} else { // geen delimiter? dan is het de laatste value,
-					$parameters[$attributeName] = $parameterString; // De waarde is bekend.
-					break 2;
-				}
 
-			default:
-				error('Invalid state');
-		}		
-	}
-	return $parameters;
-}
+				case 'VALUE':			
+					if (preg_match('/^([^'.$delimiter.']*)['.$delimiter.']/', $parameterString, $match)) {
+						$parameters[$attributeName] = $match[1]; // De waarde is bekend.
+						$parameterString = substr($parameterString, strlen($match[0]));
+						$state = 'NAME';
+						break;
+					} else { // geen delimiter? dan is het de laatste value,
+						$parameters[$attributeName] = $parameterString; // De waarde is bekend.
+						break 2;
+					}
 
-/**
- * Het resultaat van 2 Component->getHeaders() samenvoegen.
- * De waardes in $header1 worden aangevuld en overschreven door de waardes in header2
- *
- * @param array $headers
- * @param array|Component $component Een component of een header array
- * @return array
- */
-function merge_headers($headers, $component) {
-	if (is_string(SledgeHammer\array_value($headers, 'css'))) {
-		$headers['css'] = array($headers['css']);
+				default:
+					error('Invalid state');
+			}		
+		}
+		return $parameters;
 	}
 
-	if (is_array($component)) { // Is er een header array meegegeven i.p.v. een Component?
-		$appendHeaders = $component;
-	} elseif (method_exists($component, 'getHeaders')) {
-		$appendHeaders = $component->getHeaders();
-	} else {
-		return $headers; // Er zijn geen headers om te mergen.
+	/**
+	 * Het resultaat van 2 Component->getHeaders() samenvoegen.
+	 * De waardes in $header1 worden aangevuld en overschreven door de waardes in header2
+	 *
+	 * @param array $headers
+	 * @param array|Component $component Een component of een header array
+	 * @return array
+	 */
+	function merge_headers($headers, $component) {
+		if (is_string(array_value($headers, 'css'))) {
+			$headers['css'] = array($headers['css']);
+		}
+
+		if (is_array($component)) { // Is er een header array meegegeven i.p.v. een Component?
+			$appendHeaders = $component;
+		} elseif (method_exists($component, 'getHeaders')) {
+			$appendHeaders = $component->getHeaders();
+		} else {
+			return $headers; // Er zijn geen headers om te mergen.
+		}
+		foreach ($appendHeaders as $category => $values) {
+			switch ($category) {
+
+				case 'title':
+					$headers['title'] = $values;
+					break;
+
+				case 'css':
+				case 'javascript':
+					if (is_string($values)) {
+						$values = array($values);
+					}
+					if (empty($headers[$category])) {
+						$headers[$category] = $values;
+					} else {
+						$headers[$category] = array_merge($headers[$category], $values);
+					}
+					break;
+
+				default:
+					if (!is_array($values)) {
+						notice('Invalid "'.$category.'" header: values not an array, but a '.gettype($values), array('values' => $values));
+					} elseif (empty($headers[$category])) {
+						$headers[$category] = $values;
+					} else {
+						$headers[$category] = array_merge($headers[$category], $values);
+					}
+					break;
+			}
+		}
+		return $headers;
 	}
-	foreach ($appendHeaders as $category => $values) {
-		switch ($category) {
 
-			case 'title':
-				$headers['title'] = $values;
-				break;
-
-			case 'css':
-			case 'javascript':
-				if (is_string($values)) {
-					$values = array($values);
-				}
-				if (empty($headers[$category])) {
-					$headers[$category] = $values;
-				} else {
-					$headers[$category] = array_merge($headers[$category], $values);
-				}
-				break;
-
-			default:
-				if (!is_array($values)) {
-					notice('Invalid "'.$category.'" header: values not an array, but a '.gettype($values), array('values' => $values));
-				} elseif (empty($headers[$category])) {
-					$headers[$category] = $values;
-				} else {
-					$headers[$category] = array_merge($headers[$category], $values);
-				}
-				break;
+	/**
+	 * Stel de $parameters['class'] in of voegt de $class toe aan de $parameters['class'] 
+	 * 
+	 * @param string $class
+	 * @param array $parameters 
+	 * @return void
+	 */
+	function append_class_to_parameters($class, &$parameters) {
+		if (isset($parameters['class'])) {
+			$parameters['class'] .= ' '.$class;
+		} else {
+			$parameters['class'] = $class;
 		}
 	}
-	return $headers;
-}
 
-/**
- * Stel de $parameters['class'] in of voegt de $class toe aan de $parameters['class'] 
- * 
- * @param string $class
- * @param array $parameters 
- * @return void
- */
-function append_class_to_parameters($class, &$parameters) {
-	if (isset($parameters['class'])) {
-		$parameters['class'] .= ' '.$class;
-	} else {
-		$parameters['class'] = $class;
+	/** 
+	 * ID and NAME must begin with a letter ([A-Za-z]) and may be followed by any number of letters, digits ([0-9]), hyphens ("-"), underscores ("_"), colons (":"), and periods (".").
+	 */
+	function tidy_id($cdata) {
+		$cdata = trim($cdata);
+		$cdata = str_replace(array('[', ']'), '.', $cdata);
+		return $cdata;
 	}
-}
 
-/** 
- * ID and NAME must begin with a letter ([A-Za-z]) and may be followed by any number of letters, digits ([0-9]), hyphens ("-"), underscores ("_"), colons (":"), and periods (".").
- */
-function tidy_id($cdata) {
-	$cdata = trim($cdata);
-	$cdata = str_replace(array('[', ']'), '.', $cdata);
-	return $cdata;
-}
-
-/**
- * Sends the contents of the file (if necessary) including headers
- * 
- * @param string $filename Filename including path.
- * @return bool
- */
-function mirror_file($filename) {
-	deprecated('Use render_file()');
-	return render_file($filename);
-}
-
-/**
- * Genereer een <script src=""> tag, mits deze al een keer gegenereerd is.
- * @param string $src
- * @param string $identifier
- * @return void
- */
-function javascript_once($src, $identifier = null) {
-	if ($identifier === null) {
-		$identifier = $src;
+	/**
+	 * Genereer een warning() voor de EerrorHandler en verstuur de foutmelding in Json formaat naar de browser.
+	 * Deze dient dan via javascript de error te tonen. Met bv: showError(result.errorMsg)
+	 * 
+	 * @param string|Exception $errorMsg  De foutmelding string
+	 * @param string $messageVarname  De naam van de variabele die gebruikt wordt in de json response voor de foutmelding. (ext_improved.js gaat uit van "errorMsg")
+	 * @return Json
+	 */
+	function jsonError($errorMsg, $messageVarname = 'errorMsg') {
+		if ($errorMsg instanceof Exception) {
+			ErrorHandler::handle_exception($errorMsg);
+			$errorMsg = $errorMsg->getMessage();
+		} else {
+			warning('JsonError: '. $errorMsg);
+		}
+		return new Json(array(
+			'success' => false,
+			$messageVarname => $errorMsg
+		));
 	}
-	if (isset($GLOBALS['included_javascript'][$identifier])) {
-		return;
-	}
-	$GLOBALS['included_javascript'][$identifier] = true;
-	echo '<script type="text/javascript" src="'.$src.'"></script>'."\n";
-}
 
-/**
- * Genereer een warning() voor de EerrorHandler en verstuur de foutmelding in Json formaat naar de browser.
- * Deze dient dan via javascript de error te tonen. Met bv: showError(result.errorMsg)
- * 
- * @param string|Exception $errorMsg  De foutmelding string
- * @param string $messageVarname  De naam van de variabele die gebruikt wordt in de json response voor de foutmelding. (ext_improved.js gaat uit van "errorMsg")
- * @return Json
- */
-function jsonError($errorMsg, $messageVarname = 'errorMsg') {
-	if ($errorMsg instanceof Exception) {
-		ErrorHandler::handle_exception($errorMsg);
-		$errorMsg = $errorMsg->getMessage();
-	} else {
-		warning('JsonError: '. $errorMsg);
+	/**
+	 * Short for "new Json(array('success' => true))"
+	 * @param array $data (optioneel) Gegevens die naast de success worden meegestuurd.
+	 */
+	function jsonSuccess($data = array()) {
+		return new Json(array_merge(
+			$data,
+			array('success' => true)
+		));
 	}
-	return new Json(array(
-		'success' => false,
-		$messageVarname => $errorMsg
-	));
-}
-
-/**
- * Short for "new Json(array('success' => true))"
- * @param array $data (optioneel) Gegevens die naast de success worden meegestuurd.
- */
-function jsonSuccess($data = array()) {
-	return new Json(array_merge(
-		$data,
-		array('success' => true)
-	));
 }
 ?>
