266b98d8d32adb48b11344b9222a18e72cefd155
Bob Fanger <bfanger@gmail.com>
2011-11-07 03:51:07 +0100

    Upgraded JsonCrudController to CrudController Now uses the Repository and can output xml as well as json

---
+++ classes/XML.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * XML
+ * @package
+ */
+namespace SledgeHammer;
+class XML extends Object implements Document {
+
+	/**
+	 * @var SimpleXMLElement|DOMDocument
+	 */
+	private $xml;
+	
+	function __construct($xml) {
+		$this->xml = $xml;
+	}
+	
+	function render() {
+		if ($this->xml instanceof \SimpleXMLElement) {
+//			echo $this->xml->saveXML();
+			$dom = dom_import_simplexml($this->xml)->ownerDocument;
+			$dom->formatOutput = true;
+			echo $dom->saveXML();
+		} else {
+			echo $this->xml;
+		}
+	}
+	
+	/**
+	 *
+	 * @param array $data 
+	 * @return \SimpleXMLElement
+	 */
+	static function build($data) {
+		reset($data);
+		$root = key($data);
+		$xml = new \SimpleXMLElement('<?xml version="1.0" encoding="utf-8"?><'.$root.' />');
+		self::addNodes($xml, current($data), $root);
+		return $xml;
+	}
+
+	/**
+	 * @param \SimpleXMLElement $xml
+	 * @param array $data 
+	 */
+	static function addNodes($xml, $data, $node) {
+		foreach ($data as $key => $value) {
+			if (is_array($value)) {
+				if (is_int($key)) {
+					$key = $node;
+				}
+				$tree = $xml->addChild($key);
+				self::addNodes($tree, $value, $key);
+			} else {
+				$xml->$key = $value;
+			}
+		}
+	}
+
+	function isDocument() {
+		return true;
+	}
+
+	function getHeaders() {
+		return array('http' => array(
+			'Content-Type' => 'text/xml',
+		));
+	}
+}
+
+?>
