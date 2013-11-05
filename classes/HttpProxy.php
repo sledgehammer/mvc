ca0151d069013a4edc01124c2507832184ea221b
Bob Fanger <bob.fanger@noprotocol.nl>
2013-11-05 16:14:17 +0100

    HttpProxy now also uses Curl and also uses same method (GET/POST/PUT) as the current request.

--- classes/HttpProxy.php
+++ classes/HttpProxy.php
@@ -10,7 +10,6 @@ namespace Sledgehammer;
  */
 class HttpProxy extends Object implements View {
 
-	private $error = false;
 	private $headers = array();
 	private $contents;
 
@@ -18,59 +17,32 @@ class HttpProxy extends Object implements View {
 		if (substr($url, 0, 7) !== 'http://' && substr($url, 0, 8) !== 'https://') {
 			throw new \Exception('Not a valid url');
 		}
-		$this->contents = file_get_contents($url);
-		if ($this->contents !== false) {
-			// Parse the headers that came with the file_get_contents() call.
-			foreach ($http_response_header as $header) {
-				if (preg_match('/^HTTP\/1.[01](.+)$/', $header, $match)) {
-					$this->headers = array(); // drop (redirect) headers
-					$this->headers['Status'] = ltrim($match[1]);
-					continue;
-				}
-				$pos = strpos($header, ':');
-				$name = substr($header, 0, $pos);
-				$this->headers[$name] = ltrim(substr($header, $pos + 1));
-			}
-			if (count($this->headers) === 0) {
-				throw new \Exception('No HTTP headers');
-			}
-		} else { // An error occurred
-			if (isset($http_response_header)) {
-				// Forward the http error status
-				foreach ($http_response_header as $header) {
-					if (preg_match('/^HTTP\/1.[01](.+)$/', $header, $match)) {
-						$status = intval($match[1]);
-						if ($status >= 400) { // Skip 30x redirects
-							$this->error = new HttpError($status);
-						}
-					}
-				}
-			}
-			if ($this->error === false) {
-				$this->error = new HttpError(500);
-			}
+		$options = array(
+			CURLOPT_CUSTOMREQUEST => $_SERVER['REQUEST_METHOD'],
+			CURLOPT_FAILONERROR => false,
+			CURLOPT_HEADER => true,
+		);
+		if (DebugR::isEnabled()) {
+			$options[CURLOPT_HTTPHEADER] = array('DebugR: '.$_SERVER['HTTP_DEBUGR']);
 		}
+		// @todo Forward $_POST data
+
+		$response = Curl::get($url, $options);
+		$this->headers['http'] = $response->getHeaders();
+		$this->contents = $response->getBody();
+		$this->headers['http']['Status'] = $response->http_code.' '.Framework::$statusCodes[$response->http_code];
 	}
 
 	public function render() {
-		if ($this->error) {
-			$this->error->render();
-			return;
-		}
 		echo $this->contents;
 	}
 
 	function getHeaders() {
-		if ($this->error) {
-			return $this->error->getHeaders();
-		}
-		return array(
-			'http' => $this->headers
-		);
+		return $this->headers;
 	}
 
 	function isDocument() {
-		return ($this->error === false);
+		return true;
 	}
 
 }
